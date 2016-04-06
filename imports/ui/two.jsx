e252f08b4b092e525affe0c74745925b18960b75
abdou <nwabdou85@gmail.com>
2016-04-06 20:46:09 +0200

    Add chart Graph

--- imports/ui/two.jsx
+++ imports/ui/two.jsx
@@ -1,9 +1,23 @@
 
 import React, { Component} from 'react';
- import {Navigation} from './navigations.jsx'
+ import {Navigation} from './navigations.jsx';
+import ReactHighcharts from 'react-highcharts'; // Expects that Highcharts was loaded in the code. 
 
 
-
+let config = {
+  // chart: {
+  //   polar: true
+  // },
+   title: {
+    text: 'calories = f(t)'
+  },
+  xAxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+  series: [{
+    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+  }]
+};
 
 // Task component - represents a single todo item
 export default class Two extends Component {
@@ -12,9 +26,20 @@ export default class Two extends Component {
     
 
     return (
-      <h3> 
-      Ici, on dessine le graphe de l évolution des taches lié au santé de patient au file de temps  </h3>
+      <div> 
+       <h3>
+        ci-dessus c est le graphe qui represente la variation des calories en fonction de temps 
+        </h3> 
+         <ReactHighcharts config={config} ref="chart"></ReactHighcharts>
+      </div>
     );
   }
 }
  
+
+
+//var ReactHighcharts = require('react-highcharts'); 
+//var config = {
+  /* HighchartsConfig */
+//};
+//React.render(<ReactHighcharts config = {config}></ReactHighcharts>, document.body);
